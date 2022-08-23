fe2dc1d45b9919d7b2c7a8eb0a7dcd5339e5f08e
rimq14 <85926783+rimq14@users.noreply.github.com>
2022-08-23 14:26:49 +0800

    Update main.py

--- main.py
+++ main.py
@@ -36,11 +36,11 @@ def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
   return delta.days
 
-def get_birthday():
-  next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
-  if next < datetime.now():
-    next = next.replace(year=next.year + 1)
-  return (next - today).days
+# def get_birthday():
+#   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+#   if next < datetime.now():
+#     next = next.replace(year=next.year + 1)
+#   return (next - today).days
 
 def get_words():
   words = requests.get("https://api.shadiao.pro/chp")
@@ -63,7 +63,7 @@ data = {
   "windClass":{'value':winclass},
   "windDir":{"vaule":windir},
   "anniversary":{"value":get_count()},
-  "birthday":{"value":get_birthday()},
+#   "birthday":{"value":get_birthday()},
   "words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
